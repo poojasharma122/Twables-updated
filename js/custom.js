578d47a021f94ccdbb9e02f88d235003fd85e3c3
nishant <nd@int.iqsay.com>
2025-06-06 17:39:12 +0530

    add contact page

--- js/custom.js
+++ js/custom.js
@@ -34,35 +34,7 @@ var showcaseSwiper = new Swiper(".showcase-slider", {
 });
 
 
-var swiper = new Swiper('.awards-slider', {
-  slidesPerView: 8, 
-  spaceBetween: 42, 
-  autoplay: {
-    delay: 1500, 
-    disableOnInteraction: false, // Keep autoplaying after interactions
-  },
-  loop: true, 
-  breakpoints: {
-    320: {
-      slidesPerView: 3,
-    },
-    575: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 4,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1200: {
-      slidesPerView: 6,
-    },
-    1600: {
-      slidesPerView: 8,
-    },
-  },
-});
+
 
 
 var swiper = new Swiper(".popular-areas-slider", {
